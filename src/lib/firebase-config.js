b5451fd0bd3902107372f3573fc47a59ab83d6ee
Farelis30 <farelyudistira01@gmail.com>
2024-07-06 16:38:24 +0700

    deploy firebase sensor web

--- src/lib/firebase-config.js
+++ src/lib/firebase-config.js
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getDatabase } from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -22,4 +21,3 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const database = getDatabase(app);
-const analytics = getAnalytics(app);
